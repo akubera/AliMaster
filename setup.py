a2624b6ce08321f1d3a45f30c8ef7c9ed815f5dd
Andrew Kubera <andrewkubera@gmail.com>
2014-11-14 22:51:33 -0500

    Added tkinter to the setup.py requires list, also added classifiers for future pypi use.

--- setup.py
+++ setup.py
@@ -17,8 +17,20 @@ setup(       name = 'AliMaster',
  CERN's grid through AliMonitor. It currently uses the commands provided by the AliEn
  software package. Perhaps one day there will be python bindings to the services, but until
  then, AliEn (and AliROOT, for that matter) should be installed before AliMaster (to do so,
- follow the guide at https://dberzano.github.io/alice/install-aliroot.""",
+ follow the guide at https://dberzano.github.io/alice/install-aliroot.)""",
+
          packages = ['alimaster'],
-          scripts = ['scripts/alimaster']
- 
+          scripts = ['scripts/alimaster'],
+
+ install_requires = ['tkinter'],
+
+      classifiers = [
+        "Development Status :: 2 - Pre-Alpha",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Topic :: Scientific/Engineering :: Physics",
+        "Natural Language :: English"
+      ]
 )
