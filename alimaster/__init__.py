d7f4842263edcb05dda9e746dd56220c385ca1d2
Andrew Kubera <andrewkubera@gmail.com>
2014-11-14 22:42:39 -0500

    Setup.py might actually install now.

--- alimaster/__init__.py
+++ alimaster/__init__.py
@@ -2,8 +2,12 @@
 # alimaster/__init__.py
 #
 #
-"""
-HALP!!!
+"""# AliMaster
+AliMaster intends to be a utility for managing files and jobs on
+CERN's grid through AliMonitor. It currently uses the commands provided by the AliEn
+software package. Perhaps one day there will be python bindings to the services, but until
+then, AliEn (and AliROOT, for that matter) should be installed before AliMaster (to do so,
+follow the guide at https://dberzano.github.io/alice/install-aliroot.
 """
 
 __version__ = '0.0.1'
--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ from alimaster import *
 setup(       name = 'AliMaster',
           version = __version__,
     	     author = __author__,
+   	 author_email = 'andrew.michael.kubera@cern.ch',
               url = 'http://github.com/akubera/AliMaster',
           license = 'LGPLv3+',
       description = "A (non-web) graphic user interface for interacting with AliMonitor",
@@ -17,6 +18,7 @@ setup(       name = 'AliMaster',
  software package. Perhaps one day there will be python bindings to the services, but until
  then, AliEn (and AliROOT, for that matter) should be installed before AliMaster (to do so,
  follow the guide at https://dberzano.github.io/alice/install-aliroot.""",
- 
+         packages = ['alimaster'],
+          scripts = ['scripts/alimaster']
  
 )
