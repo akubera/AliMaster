bbc02e6e012c0bcd7557a8a932c029bacbeb465e
Andrew Kubera <andrewkubera@gmail.com>
2014-12-10 22:49:00 -0500

    Updated style.py to include the previous settings.

--- alimaster/gui/style.py
+++ alimaster/gui/style.py
@@ -9,11 +9,33 @@ from tkinter.ttk import *
 style = ttk.Style()
 
 style.theme_create("alimaster", parent='alt', settings={
-  "TNotebook": {"configure": {"tabmargins": [2, 5, 2, 0]}},
+
+  "TNotebook" : {
+    "configure": {"tabmargins": [2, 5, 2, 0]}
+  },
+
   "TNotebook.Tab": {
     "configure": {"padding": [5, 1]}
   },
   "alienwindow.TFrame" : {
     "configure": {"padding": [5, 1]}
+  },
+
+  "StatusGood.TLabel" : {
+    "configure": {"foreground": "green"}
+  },
+  "StatusBad.TLabel" : {
+    "configure": {"foreground": "red"}
+  },
+
+  "Sidebar_FileBrowser.TFrame": {
+    "configure": {"background": "blue"}
+  },
+
+  "Toolbar.TButton": {
+    "configure": {"foreground": "red",
+                  "background": "black",
+                      "height": 64}
   }
+
 })
