05911289b3ff3a79d101d7b7464cdc5a24abe8c0
Andrew Kubera <andrewkubera@gmail.com>
2015-03-30 17:18:07 +0200

    Removed long-description, as that should be read from README.rst

--- setup.py
+++ setup.py
@@ -44,12 +44,6 @@ setup(
     url=metadata.url,
     license=metadata.license,
     description=desc,
-    long_description="""AliMaster intends to be a utility for managing files
- and jobs on CERN's grid through AliMonitor. It currently uses the commands
- provided by the AliEn software package. Perhaps one day there will be python
- bindings to the services, but until then, AliEn (and AliROOT, for that matter)
- should be installed before AliMaster (to do so, follow the guide at
- https://dberzano.github.io/alice/install-aliroot.)""",
     packages=PACKAGES,
     scripts=SCRIPTS,
     install_requires=REQUIRES,
