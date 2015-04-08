c0f5aaf3579e226aa459872625beaf4266bdfa02
Andrew Kubera <andrewkubera@gmail.com>
2015-04-08 08:35:12 +0200

    Formatting in the style-file

--- alimaster/gui/style.py
+++ alimaster/gui/style.py
@@ -18,54 +18,76 @@ def get_style():
     return style
 
 style_theme = {
-  "." : {
-    "configure": {
-      # "font": ('Helvetica', 50)
+    "." : {
+        "configure": {
+          # "font": ('Helvetica', 50)
+        }
+    },
+
+    "TNotebook" : {
+        "configure": {
+            "tabmargins": [2, 5, 2, 0]
+        }
+    },
+
+    "TNotebook.Tab": {
+        "configure": {
+            "padding": [5, 1]
+        }
+    },
+    "alienwindow.TFrame" : {
+        "configure": {
+            "padding": [5, 1]
+        }
+    },
+
+    "StatusGood.TLabel" : {
+        "configure": {
+            "foreground": "green"
+        }
+    },
+
+    'StatusBad.TLabel' : {
+        'configure': {
+            'foreground': 'red'
+        }
+    },
+
+    "Sidebar_FileBrowser.TFrame": {
+        "configure": {
+            'background': 'blue'
+        }
+    },
+
+    # This doesn't work (yet)
+    'Awesome.Toolbar.TButton': {
+        'configure': {
+            'font': fontawesome
+        }
+    },
+
+    'TButton': {
+        'configure': {
+            'relief': 'raised',
+            'cursor':'help',
+            'anchor':'center',
+            'padding': 3
+        },
+        'map': {
+            'foreground': [
+                ('disabled', '#999'),
+                ('pressed', '#444'),
+                ('active', '#444')
+            ]
+        }
+    },
+
+
+    "Toolbar.TButton": {
+        'configure': {
+            'relief': 'flat',
+            'font': ('DejaVu Mono', -13)
+        }
     }
-  },
-
-  "TNotebook" : {
-    "configure": {"tabmargins": [2, 5, 2, 0]}
-  },
-
-  "TNotebook.Tab": {
-    "configure": {"padding": [5, 1]}
-  },
-  "alienwindow.TFrame" : {
-    "configure": {"padding": [5, 1]}
-  },
-
-  "StatusGood.TLabel" : {
-    "configure": {"foreground": "green"}
-  },
-  "StatusBad.TLabel" : {
-    "configure": {"foreground": "red"}
-  },
-
-  "Sidebar_FileBrowser.TFrame": {
-    "configure": {"background": "blue"}
-  },
-
-  "Toolbar": {
-    "configure" : {}
-  },
-
-  # This doesn't work (yet)
-  "Awesome.Toolbar.TButton": {
-    "configure": {'font': fontawesome}
-  },
-
-  "Toolbar.TButton": {
-    "configure": {"relief": "flat",
-                'font': ('DejaVu Mono', -13)}
-  },
-
-  "TButton": {
-    "configure": {"relief": "raised",
-                "cursor":"help",
-                'anchor':'center',
-                'padding': 3},
-    "map": {"foreground": [("pressed", '#444'), ('active', '#444')]}
-  }
 
 }
