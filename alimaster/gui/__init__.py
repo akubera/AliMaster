a1d57b6f9f4fa79fa094f1cbf2ada9440b7d24bd
Andrew Kubera <andrewkubera@gmail.com>
2015-04-08 11:28:41 +0200

    Moved the imports around in gui to provide easy imports for submodules.

--- alimaster/gui/__init__.py
+++ alimaster/gui/__init__.py
@@ -5,7 +5,8 @@
 from os import path
 from .. import __version__
 
+from .utils import RES
+
+from .fontawesome import FontAwesome
 from .mainwindow import MainWindow
 from .simple_window import SimpleWindow
-from .utils import RES
-from .fontawesome import FontAwesome
