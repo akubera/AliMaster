18314f68eb611db2b5a857846fbbcf8849ffb0ea
Andrew Kubera <andrewkubera@gmail.com>
2015-04-09 13:46:32 +0200

    Analysis builder main window creates a toolbar and statusbar

--- alimaster/analysis_builder/gui/main_window.py
+++ alimaster/analysis_builder/gui/main_window.py
@@ -22,7 +22,10 @@ class MainWindow(SimpleWindow):
     img_size = 14
 
     imgs = {
-        'save': FontAwesome.generate_icon('search', img_size)
+        'new': FontAwesome.generate_icon('file-o', img_size),
+        'open': FontAwesome.generate_icon('folder-o', img_size),
+        'save': FontAwesome.generate_icon('floppy-o', img_size),
+        'new_project': FontAwesome.generate_icon('cube', img_size),
     }
 
     def __init__(self, builder, root):
@@ -32,3 +35,36 @@ class MainWindow(SimpleWindow):
         @param root: TK object to be the root of this window
         """
         super().__init__(root, "AliMaster | Analysis Builder")
+        self.window = self.frame
+        self._setup_toolbar()
+        self.frame = Frame(self.window)
+        self._setup_statusbar()
+        self.frame.pack(expand=True, fill=BOTH)
+
+
+    def _setup_toolbar(self):
+        self.toolbar = Frame(self.window, relief=RAISED)
+        def add_button(img_name, command=None):
+            Button(self.toolbar,
+                   style='Toolbar.TButton',
+                   image=self.imgs[img_name],
+                   command=lambda:print(img_name)if command is None else command).pack(side=LEFT, padx=0, pady=2)
+
+        add_button('new_project')
+        Separator(self.toolbar,orient=VERTICAL).pack(side=LEFT, padx=1, pady=4, fill=Y)
+        add_button('new')
+        add_button('open')
+        add_button('save')
+
+        self.toolbar.pack(side=TOP, fill=X, expand=0)
+
+    def _setup_statusbar(self):
+        self.status = StringVar()
+        self.statusbar = Label(self.window,
+                               textvar=self.status,
+                               font=("DejaVu Sans", -10),
+                               relief='sunken'
+                               )
+        self.set_status = self.status.set
+        self.set_status("")
+        self.statusbar.pack(side=BOTTOM, fill=X, expand=0, pady=1)
