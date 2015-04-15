0e75e5a0ce280580b0bcd223bed1ead53aad6135
Andrew Kubera <andrewkubera@gmail.com>
2015-04-15 18:23:53 +0200

    MainWindow now minimizes upon close if it has children. The file is now pep8.

--- alimaster/gui/mainwindow.py
+++ alimaster/gui/mainwindow.py
@@ -15,97 +15,117 @@ import alimaster
 import threading
 from threading import Thread
 
-class MainWindow():
 
-  def __init__(self, app):
+class MainWindow():
     """
+    The main window which holds some status about the ALIEN connection and
+    provides buttons which load all the other alimaster windows.
+
+    If there are other child windows open, the MainWindow will not close, but
+    minimize to taskbar.
+    """"
+
+    def __init__(self, app):
+        """
         Create the main 'control' window of the AliMaster program
         """
-    self.app = app
-    self.root = app.root
+        self.app = app
+        self.root = app.root
 
-    self.window = app.get_new_window("Alimaster Control", (220, 500))
-    self.window.protocol("WM_DELETE_WINDOW", self.close)
+        self.window = app.get_new_window("Alimaster Control", (220, 500))
+        self.window.protocol("WM_DELETE_WINDOW", self.close)
 
-    # self.window = Toplevel(master)
-    # self.window.minsize(220,500)
-    # self.window.title("Alimaster Control")
-    # self.window.protocol("WM_DELETE_WINDOW", self.hide)
+        # self.window = Toplevel(master)
+        # self.window.minsize(220,500)
+        # self.window.title("Alimaster Control")
+        # self.window.protocol("WM_DELETE_WINDOW", self.hide)
 
-    #self.style = Style()
-    #GenerateStyle(self.style)
-    # status_style.configure("StatusGood.TLabel", foreground="green")
-    # status_style.configure("StatusBad.TLabel", foreground="red")
+        # self.style = Style()
+        # GenerateStyle(self.style)
+        # status_style.configure("StatusGood.TLabel", foreground="green")
+        # status_style.configure("StatusBad.TLabel", foreground="red")
 
-    self.frame = Frame(self.window)
-    self.status_bar = Frame(self.frame)
-    self.status_bar.label = Label(self.status_bar, text="Status")
-    self.status_bar.label.pack(side=LEFT)
-    self.status_bar.status = Label(self.status_bar, text="●")
-    self.status_bar.status.pack()
+        self.frame = Frame(self.window)
 
-    self.label = Label(self.frame, text="AliMaster v%s" % (__version__), font=('DejaVu Mono', 16)).pack(pady=9, padx=4)
-    self.status_bar.pack(fill=X, pady=(9,3), padx=4)
+        self.status_bar = Frame(self.frame)
 
-    self.file_browser_button = Button(self.frame,
-                                      text="File Browser",
-                                      command=self.create_filebrowser)
-    self.file_browser_button.pack(fill=X, pady=(9,3), padx=4)
+        self.status_bar.label = Label(self.status_bar,
+                                      text="Status"
+                                      ).pack(side=LEFT)
 
+        self.status_bar.status = Label(self.status_bar,
+                                       text="●"
+                                       ).pack()
 
-    self.settings_button = Button(self.frame,
-                                  text="Settings",
-                                  command=self.create_settings_window)
-    self.settings_button.pack(fill=X, pady=(9,3), padx=4)
+        self.label = Label(self.frame,
+                           text="AliMaster v%s" % (__version__),
+                           font=('DejaVu Mono', 16)
+                           ).pack(pady=9, padx=4)
 
-    self.help_button = Button(self.frame,
-                              text="Help",
-                              command=self.create_helpwindow)
+        self.status_bar.pack(fill=X, pady=(9, 3), padx=4)
 
-    self.load = Button(self.frame, text="Load ROOT", command=self.load_root)
-    self.quit = Button(self.frame, text="Quit", command=self.app.quit)
+        self.file_browser_button = Button(self.frame,
+                                          text="File Browser",
+                                          command=self.create_filebrowser
+                                          ).pack(fill=X, pady=(9, 3), padx=4)
 
-    self.help_button.pack(fill=X, pady=(9,3), padx=4)
-    self.load.pack(fill=X, pady=(3,3), padx=4)
-    self.quit.pack(fill=X, pady=(3,9), padx=4)
-    self.set_status_good()
+        self.settings_button = Button(self.frame,
+                                      text="Settings",
+                                      command=self.create_settings_window
+                                      ).pack(fill=X, pady=(9, 3), padx=4)
 
-    self.frame.pack(fill=BOTH, expand=1)
+        self.help_button = Button(self.frame,
+                                  text="Help",
+                                  command=self.create_helpwindow
+                                  ).pack(fill=X, pady=(9, 3), padx=4)
 
-  def quit(self):
-    print ("[MainWindow::quit]")
-    self.root.after(0, self.root.quit)
+        self.load_button = Button(self.frame,
+                                  text="Load ROOT",
+                                  command=self.load_root
+                                  ).pack(fill=X, pady=(3, 3), padx=4)
+        self.quit_button = Button(self.frame,
+                                  text="Quit",
+                                  command=self.app.quit
+                                  ).pack(fill=X, pady=(3, 9), padx=4)
 
-  def hide(self):
-    self.window.withdraw()
+        self.set_status_good()
+        self.frame.pack(fill=BOTH, expand=1)
 
-  def show(self):
-    self.window.update()
+    def quit(self):
+        print("[MainWindow::quit]")
+        self.root.after(0, self.root.quit)
 
-  def close(self):
+    def hide(self):
+        self.window.withdraw()
+
+    def show(self):
+        self.window.update()
+
+    def close(self):
         print('window_count', self.app._win_count)
         if self.app._win_count < 2:
             self.app.quit()
-        # self.hide()
+        else:
+            self.window.iconify()
 
-  def create_filebrowser(self):
+    def create_filebrowser(self):
         FileBrowserWindow(self.app)
 
-  def create_helpwindow(self):
+    def create_helpwindow(self):
         HelpWindow(self.app)
 
-  def create_settings_window(self):
+    def create_settings_window(self):
         SettingsWindow(self.app)
 
-  def set_status_bad(self):
-    self.status_bar.status.configure(style='StatusBad.TLabel')
+    def set_status_bad(self):
+        self.status_bar.status.configure(style='StatusBad.TLabel')
 
-  def set_status_good(self):
-    from .style import style
-    self.status_bar.status.configure(style='StatusGood.TLabel')
+    def set_status_good(self):
+        from .style import style
+        self.status_bar.status.configure(style='StatusGood.TLabel')
 
-  def run_in_thread():
-    pass
+    def run_in_thread():
+        pass
 
-  def load_root(self):
+    def load_root(self):
         alimaster.import_root_module()
