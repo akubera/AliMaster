6ad511cadf3681e0dfc1b2b1adb6108a166e7a10
Andrew Kubera <andrewkubera@gmail.com>
2015-05-12 21:03:28 -0400

    Arbitrary version bump to v0.0.4

--- alimaster/metadata.py
+++ alimaster/metadata.py
@@ -6,7 +6,7 @@
 package = 'alimaster'
 project = 'A Python Module for interfacing with the ALICE experiment'
 
-version_info = (0, 0, 3)
+version_info = (0, 0, 4)
 version = '.'.join(map(str, version_info))
 version_name = ' '.join([project, 'version', version])
 
@@ -14,7 +14,7 @@ author = "Andrew Kubera"
 author_email = "andrew.michael.kubera@cern.ch"
 copyright = "Copyright 2015, Andrew Kubera"
 
-date = "Mar 30, 2015"
+date = "May 12, 2015"
 
 license = "LGPLv3+"
 url = 'http://github.com/akubera/AliMaster'
