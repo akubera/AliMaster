4924a11238d082925401f805a5c2afaa331d8740
Andrew Kubera <andrewkubera@gmail.com>
2015-07-07 13:08:52 -0400

    Added emtpy ClassPane view

---
+++ alimaster/analysis_builder/gui/class_pane.py
@@ -0,0 +1,22 @@
+#
+# alimaster/manalysis_builder/gui/class_pane.py
+#
+"""
+Module for building the gui component which presents a panel with all options
+to edit a class.
+"""
+
+from tkinter import *       # noqa
+from tkinter.ttk import *   # noqa
+
+
+class ClassPane:
+
+    def __init__(self, parent, ali_class):
+        pass
+
+    def _build_interface(self):
+        pass
+
+    def close(self):
+        pass
