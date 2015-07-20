aa8ea352dccea13e813c72883f6583e2d8455967
Andrew Kubera <andrewkubera@gmail.com>
2015-07-19 23:14:44 -0400

    RootSelector : PEP8 fixes for main window file

--- alimaster/root_selector/gui/main_window.py
+++ alimaster/root_selector/gui/main_window.py
@@ -5,19 +5,17 @@
 The main window for the root_selector alimaster subpackage.
 """
 
-import asyncio
 import threading
 import subprocess
 import os
 
-from tkinter import *
-from tkinter.ttk import *
-
-from PIL import Image, ImageTk
+from tkinter import *                                                    # noqa
+from tkinter.ttk import *                                                # noqa
 
 from alimaster.root_selector.finder import Finder
 from alimaster.gui.fontawesome import FontAwesome
 from alimaster.gui import SimpleWindow
+
 
 class MainWindow(SimpleWindow):
 
@@ -31,7 +29,7 @@ class MainWindow(SimpleWindow):
         """
         Construct the main window of the root_selector program
         """
-        print ("[MainWindow::__init__]")
+        print("[MainWindow::__init__]")
         super().__init__(root, "AliMaster | ROOT Selector", (400, 200))
 
         search_img = self.imgs['search']
@@ -45,9 +43,9 @@ class MainWindow(SimpleWindow):
                                             command=self.search_for_aliroot)
         self.aliroot_search_button.pack(side=LEFT)
 
-        self.aliroot_lbl = Label(self.aliroot_frame , text="AliRoot")
+        self.aliroot_lbl = Label(self.aliroot_frame, text="AliRoot")
         self.aliroot_lbl.pack(side=LEFT)
-        self.aliroot_frame.pack(side=TOP, fill=X, pady=(6,0), padx=8)
+        self.aliroot_frame.pack(side=TOP, fill=X, pady=(6, 0), padx=8)
 
         self.alirootlist = Treeview(self.frame)
         self.alirootlist['columns'] = ('version')
@@ -69,13 +67,13 @@ class MainWindow(SimpleWindow):
                                          )
         self.root_search_button.pack(side=LEFT)
 
-        self.aliroot_lbl = Label(self.root_frame , text="ROOT")
+        self.aliroot_lbl = Label(self.root_frame, text="ROOT")
         self.aliroot_lbl.pack(side=LEFT)
-        self.root_frame.pack(side=TOP, fill=X, pady=(6,0), padx=8)
+        self.root_frame.pack(side=TOP, fill=X, pady=(6, 0), padx=8)
 
         self.rootlist = Treeview(self.frame)
         self.rootlist.heading('#0', text='ROOTSYS')
-        self.rootlist['columns'] = ('version') # , 'modified', 'owner')
+        self.rootlist['columns'] = ('version')  # , 'modified', 'owner')
         self.rootlist.heading('version', text='version')
         # self.rootlist.heading('owner', text='owner')
         self.rootlist.pack(fill=BOTH, expand=1, padx=4, pady=5)
@@ -92,10 +90,10 @@ class MainWindow(SimpleWindow):
         # master = self.frame
         # self.btn = Button(master , text = "Button" , command = self.command )
         # self.btn.pack()
-        print ("[MainWindow::__init__] Done.")
+        print("[MainWindow::__init__] Done.")
 
     def command(self):
-        print ('Button is pressed!')
+        print('Button is pressed!')
 
         self.newWindow = tk.Toplevel(self.master)
         self.app = windowclass1(self.newWindow)
@@ -103,6 +101,7 @@ class MainWindow(SimpleWindow):
     def search_for_aliroot(self):
         finder = Finder()
         aliroot = self.open_searchbox(finder.find_aliroot)
+        return aliroot
 
     def search_for_root(self):
         finder = Finder()
@@ -110,7 +109,6 @@ class MainWindow(SimpleWindow):
 
     def open_searchbox(self, fcn=None):
         from tkinter import filedialog
-        from queue import Queue
 
         d = filedialog.askdirectory()
         if not d:
@@ -118,24 +116,19 @@ class MainWindow(SimpleWindow):
 
         # self.rootlist.insert('', 'end', 'ROOT', text=root_path)
 
-        q = Queue()
-
         # @asyncio.coroutine
         def threaded_find_roots():
-            print ("Searching for ROOTs in path",d)
-            # for x in range(4):
-            if fcn is None:
-                fcn = Finder.find_root
+            print("Searching for ROOTs in path", d)
 
-            for root_path in fcn(d):
+            for root_path in Finder.find_root(d):
                 meta = {}
-                exe = os.path.join(root_path,'root-config')
+                exe = os.path.join(root_path, 'root-config')
                 meta['rootsys'] = subprocess.check_output([exe, "--prefix"])
                 meta['version'] = subprocess.check_output([exe, "--version"])
                 meta['features'] = subprocess.check_output([exe, "--features"])
-                # print ('found root path:', root_path)
+                # print('found root path:', root_path)
                 self.insert_item(meta['rootsys'], meta)
-                print ("GOOD")
+                print("GOOD")
                 # yield from asyncio.sleep(1)
                 # self.rootlist.insert('','end',x, text=x )
 
@@ -144,10 +137,10 @@ class MainWindow(SimpleWindow):
 
         # asyncio.get_event_loop().run_until_complete(asyncio_find_roots())
 
-        # print ("selected",d)
+        # print("selected",d)
         # finder = Finder()
         # for root_path in finder.Find(d):
-            # self.rootlist.insert('', 'end', 'ROOT', text=root_path)
+        #     self.rootlist.insert('', 'end', 'ROOT', text=root_path)
 
         # self.searchbox = Toplevel(self.root)
         # searchframe = Frame(self.searchbox)
@@ -160,18 +153,18 @@ class MainWindow(SimpleWindow):
         from _tkinter import TclError
         key = self.rootlist.insert('', 'end', text=item)
         newitem = self.rootlist.item(key)
-        print ('key', key.__class__)
-        print ('new', newitem.__class__)
+        print('key', key.__class__)
+        print('new', newitem.__class__)
         for header, value in extras.items():
             value = value.decode()
-            print ('adding', header, ':', value)
+            print('adding', header, ':', value)
             try:
                 self.rootlist.set(key, header, value)
             except AttributeError:
                 pass
             except TclError:
                 pass
-        print ('[insert_item] DONE')
+        print('[insert_item] DONE')
 
     def set_selected_item(self):
         pass
